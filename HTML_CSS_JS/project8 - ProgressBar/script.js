2db72262afb32d8a02361f02a50aecc05a10303d
karthik <karthik.mani@factentry.com>
2026-02-27 11:19:04 +0530

    feat: add reset functionality to Progress Bar component

--- HTML_CSS_JS/project8 - ProgressBar/script.js
+++ HTML_CSS_JS/project8 - ProgressBar/script.js
@@ -6,15 +6,57 @@ const ResetBtn = document.querySelector(".reset-btn")
 // hint css
 //  background: linear-gradient(to right, green 10%, white 10%);
 
+let percent = 0;
+let color = 'orange';
+let intervalID;
+
+
+const handleProgressColor = (percent, color) => {
+
+    if (percent >= 0 && percent <= 50) {
+        color = 'pink'
+        progressContainer.style.background = ` linear-gradient(to right, ${color} ${percent}%, white ${percent}%)`
+        // progressContainer.style.transition = `background 0.5s ease-in-out`
+    }
+    else if (percent >= 51 && percent <= 80) {
+        color = 'green'
+        progressContainer.style.background = ` linear-gradient(to right, ${color} ${percent}%, white ${percent}%)`
+        // progressContainer.style.transition = `background 0.5s ease-in-out`
+    }
+    progressContainer.style.background = ` linear-gradient(to right, ${color} ${percent}%, white ${percent}%)`
+}
+
 
 const handleProgress = (event) => {
-    const percent = 10;
-    progressContainer.style.background = ` linear-gradient(to right, green ${percent}%, white ${percent}%)`
+
+    intervalID = setInterval(() => {
+        // auto clear Interval based on percent
+        if (percent === 90) {
+            clearInterval(intervalID)
+            return
+        }
+
+        // we can play with percent and interval time , for smooth UI progress
+        percent = percent + 0.5;
+        handleProgressColor(percent, color);
+    }, 10)
+
+
+
+    // !no use here
+    // if (percent === 90) {
+    //     clearInterval(intervalID)
+    // }
+}
+
+const handleResetProgress = (event) => {
+    clearInterval(intervalID)
+    percent = 0;
+    const color = 'white';
+    progressContainer.style.background = ` linear-gradient(to right, ${color} ${percent}%, white ${percent}%)`
 }
 
 
 
-
-
-
 startBtn.addEventListener("click", handleProgress)
+ResetBtn.addEventListener("click", handleResetProgress)
