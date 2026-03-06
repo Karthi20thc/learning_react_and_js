6d18aaf18b088c617c1741ee5b4b569f6acae57d
karthik <karthik.mani@factentry.com>
2026-03-06 14:45:04 +0530

    refactor prime number checker comments and add Fibonacci sequence function

---
+++ coding_problems/arrays/Fibonacci/Print the first n Fibonacci numbers.js
@@ -0,0 +1,14 @@
+function fibonacci(n) {
+    let a = 0;
+    let b = 1;
+
+    for (let i = 0; i < n; i++) {
+        console.log(a);
+
+        let next = a + b;
+        a = b;
+        b = next;
+    }
+}
+
+fibonacci(7);
