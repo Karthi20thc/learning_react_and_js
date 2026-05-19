7ad1502514044678e5fe7593ac24c96f58b6fd30
karthik <karthik.mani@factentry.com>
2026-05-19 18:08:48 +0530

    Add Java launch configuration for UsefullArrayListMethods; remove obsolete files and update ArrayList initialization documentation

---
+++ .history/coding_problems_in_java/src/Learnings/ArrayList/UsefullArrayListMethods_20260519150209.java
@@ -0,0 +1,22 @@
+package Learnings.ArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsefullArrayListMethods {
+
+    public static void main(String[] args) {
+        ArrayList<Character> myArr = new ArrayList<>(List.of('h', 'e', 'l', 'l', 'o'));
+
+        // .set 
+        myArr.set(0, 'z');
+        System.out.println(myArr);
+
+        // .get
+        System.out.println(myArr.get(2));
+        System.out.println(myArr);
+
+        System.out.println(myArr);
+
+    }
+}
