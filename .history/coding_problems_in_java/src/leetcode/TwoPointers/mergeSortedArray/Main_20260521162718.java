e3a0e699f374d08a9a787108645e8c2bf4599e38
karthik20thc <nevergiveup21thc@gmail.com>
2026-05-22 09:38:50 +0530

    Add implementation and documentation for merging two sorted arrays in Java

--- .history/coding_problems_in_java/src/leetcode/TwoPointers/mergeSortedArray/Main_20260521162718.java
+++ .history/coding_problems_in_java/src/leetcode/TwoPointers/mergeSortedArray/Main_20260521162718.java
@@ -5,15 +5,10 @@ package leetcode.TwoPointers.mergeSortedArray;
 public class Main {
 
     public static void main(String[] args) {
-        int[] nums1 = {1, 2, 3, 0, 0, 0};
-        int[] nums2 = {2, 5, 6};
-        int m = 3;
-        int n = 3;
 
-        merge(nums1, m, nums2, n);
     }
 
-    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
 
     }
 }
